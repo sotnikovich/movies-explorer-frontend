bdcf6a954c8a5dee16d843b292e0c6a4c321a756
Max Sotnikov <felaw@yandex.ru>
2022-06-26 14:04:16 +0300

    about-project

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,11 @@
+import { AboutProject } from "../AboutProject/AboutProject";
 import { Promo } from "../Promo/Promo";
 
 export const Main = () => {
   return (
     <>
       <Promo />
+      <AboutProject />
     </>
   );
 };
